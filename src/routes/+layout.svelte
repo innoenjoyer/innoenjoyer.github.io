21e32802411b0db9ec47fd02c9c0eb028cd5c790
innoenjoyer <aj@inno.tatar>
2023-07-12 02:46:42 +0300

    added usage of Svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,45 @@
+<svelte:head>
+    <link rel="stylesheet" href="/styles/style.css">
+</svelte:head>
+
+<header>
+    <div class="header-content">
+        <h1 class="header-content-logo">Portfolio</h1>
+    </div>
+</header>
+
+<slot></slot>
+
+<footer>
+    <section class="footer-section">
+        <article>
+            <h4>Site map</h4>
+            <ul>
+                <li><a href="/">My Bio</a></li>
+                <li><a href="/comic">Comic</a></li>
+            </ul>
+        </article>
+        <article>
+            <h4>Funny links</h4>
+            <ul>
+                <li><a href="http://inno.tatar">inno.tatar</a></li>
+                <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Not a rickroll</a></li>
+            </ul>
+        </article>
+        <article></article>
+    </section>
+</footer>
+<div class="tm-footer">
+    <div class="tm-footer-contacts">
+        <a href="https://github/com/anijackich">
+            <img src="/img/social/github.svg" alt="github">
+        </a>
+        <a href="https://t.me/anijack">
+            <img src="/img/social/telegram.svg" alt="telegram">
+        </a>
+        <a href="mailto:mr@anjk.ru">
+            <img src="/img/social/mail.svg" alt="mail">
+        </a>
+    </div>
+</div>
+
---
+++ src/routes/comic/+page.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+    import moment from 'moment';
+    import { onMount } from 'svelte';
+
+    const email = "e.anisov@innopolis.university" as string;
+    let comicTitle = '';
+    let comicDate = '';
+    let comicImageSrc = '';
+    let comicImageAlt = '';
+
+    interface Comic {
+        "month": string,
+        "num": number,
+        "link": string,
+        "year": string,
+        "news": string,
+        "safe_title": string,
+        "transcript": string,
+        "alt": string,
+        "img": string,
+        "title": string,
+        "day": string
+    }
+
+    type ComicId = number;
+
+    onMount(() => {
+        if (typeof document !== 'undefined') {
+            function fetchComicById(id: ComicId): Promise<Comic> {
+                const comicParams = new URLSearchParams() as URLSearchParams;
+                comicParams.append('id', String(id));
+
+                return fetch("https://fwd.innopolis.university/api/comic?" + comicParams.toString()).then(r => r.json());
+            }
+
+            function fetchIdByEmail(email: string): Promise<ComicId> {
+                const hw2Params = new URLSearchParams() as URLSearchParams;
+                hw2Params.append('email', email);
+
+                return fetch("https://fwd.innopolis.app/api/hw2?" + hw2Params.toString()).then(r => r.json());
+            }
+
+            const setComic = async () => {
+                let comic = await fetchComicById(await fetchIdByEmail(email)) as Comic;
+                comicTitle = comic['safe_title'];
+                comicDate = moment(`${comic.year}-${comic.month}-${comic.day}`).fromNow();
+                comicImageSrc = comic['img'];
+                comicImageAlt = comic['alt'];
+            }
+
+            setComic();
+        }
+    });
+</script>
+
+<svelte:head>
+    <title>Comic</title>
+</svelte:head>
+
+<section class="main-section">
+    <h1 id="comic-title">{comicTitle}</h1>
+    <p id="comic-publication-date">{comicDate}</p>
+    <img id="comic-img" src="{comicImageSrc}" alt="{comicImageAlt}">
+</section>
