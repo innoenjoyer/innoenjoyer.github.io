4f2f57a3364b6df21e41d41e6b01dcfc4d44179d
innoenjoyer <aj@inno.tatar>
2023-07-12 11:14:10 +0300

    fix svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,14 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
-const dev = process.argv.includes('dev');
+// const dev = process.argv.includes('dev');
 const config = {
 	preprocess: vitePreprocess(),
 		
 	kit: {
-		path: {
-			base: dev ? '' : process.env.BASE_PATH, 
-		},
+		// path: {
+		// 	base: dev ? '' : process.env.BASE_PATH,
+		// },
 		adapter: adapter()
 	}
 };
